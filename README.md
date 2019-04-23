37ddd7c124147e3232838f6776caf35d4d12ccfb
RenshanPang <645893871@qq.com>
2019-04-23 06:58:53 -0400

    project

--- README.md
+++ README.md
@@ -2,11 +2,11 @@ Introduction:
 This is my EE551 python individual project |Author: Renshan Pang
 
 Purposals:
-This project is mainly to use the data analysis technology to count the total births in US from 1880 to 2010. To explain it in detail, by analyzing the birth data from 1880 to 2010, we can know the total birth number of male and female from 1880 to 2010. And also, we can get the naming trends of specific names like John, Harry, Mary and so on. What's more, we can measure the increase in naming diversity by counting the proportion of some popular names in all names. 
+This project is mainly to use the Web crawler Technology and the tkinter package to form an application, which can translate the messages between Chinese and English and clear the writing table. The website I used is https://fanyi.baidu.com/, which is a Chinese translation website. The translating task actually is done by this website. What the application to do is imitating the access proccess.
 
 TODO:
-Make sure the goal of the analysis; 
-Get the original data and pre-dispose the data.; 
-Data cleaning (Exclude outliers, blank values, invalid values, duplicate values); 
-Data sorting (base on the aim of the analysis); 
-Use the results to draw the picture.
+
+1.Analyze the structure of the website
+2.Design the translation method
+3.Design clear method
+4.Design the interface of the application
---
+++ translator.py
@@ -0,0 +1,51 @@
+import requests
+import tkinter as tk
+def translateCtoE():
+    url = 'https://fanyi.baidu.com/transapi'
+    data={
+        'from':	'zh',
+        'to': 'en',
+        'query': t1.get(0.0, 'end'),
+        'transtype': 'realtime',
+        'simple_means_flag': '3',
+        'sign': '338183.117302',
+        'token': 'd376ab8d9c4598d446a3d6adf3d6c72a'
+    }
+    response = requests.post(url, data= data).json()
+    r = response['data'][0]['dst']
+    t1.delete(0.0, 'end')
+    t1.insert('end', r)
+
+def translateEtoC():
+    url = 'https://fanyi.baidu.com/transapi'
+    data={
+        'from':	'en',
+        'to': 'zh',
+        'query': t1.get(0.0, 'end'),
+        'transtype': 'realtime',
+        'simple_means_flag': '3',
+        'sign': '338183.117302',
+        'token': 'd376ab8d9c4598d446a3d6adf3d6c72a'
+    }
+    response = requests.post(url, data= data).json()
+    r = response['data'][0]['dst']
+    t1.delete(0.0, 'end')
+    t1.insert('end', r)
+
+def clear():
+    t1.delete(0.0, 'end')
+
+root = tk.Tk()
+root.title('Translation Aplication')
+root.geometry('400x200')
+l1 = tk.Label(root, text = 'Please input messages: ')
+l1.grid()
+t1 = tk.Text(root, width = 56, height = 5)
+t1.grid()
+b1 = tk.Button(root, text = 'Translating from Chinese to English', width = 32, command = translateCtoE)
+b1.grid(row = 2, column = 0)
+b2 = tk.Button(root, text = 'Translating from English to Chinese', width = 32, command = translateEtoC)
+b2.grid(row = 3, column = 0)
+b3 = tk.Button(root, text = 'Clear', width = 32, command = clear)
+b3.grid(row = 4, column = 0)
+root.mainloop()
